34373198bbd901aac20b77021e0b27b1eaf89ce7
Mars.Su <49678319+hueiyuan@users.noreply.github.com>
2022-04-12 20:10:47 +0800

    Add utils script

---
+++ project_libs/common/utils.py
@@ -0,0 +1,24 @@
+import argparse
+import boto3
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def get_ssm_value(region, ssm_name, is_decryption=True):
+    client = boto3.client(service_name='ssm', region_name=region)
+    response = client.get_parameter(
+        Name=ssm_name,
+        WithDecryption=is_decryption
+    )
+
+    corresponding_value = response['Parameter']['Value']
+
+    return corresponding_value.replace('\"','')
+  
