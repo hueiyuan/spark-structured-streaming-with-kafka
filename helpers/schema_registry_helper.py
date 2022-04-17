846a397e8483014641e0458561e07d5d663d206c
Mars.Su <49678319+hueiyuan@users.noreply.github.com>
2022-04-17 14:15:45 +0800

    Add schema registry function typing

--- helpers/schema_registry_helper.py
+++ helpers/schema_registry_helper.py
@@ -1,13 +1,17 @@
 from confluent_kafka.schema_registry import SchemaRegistryClient
 
 class SRClient:
-    def __init__(self, schema_registry_url, schema_registry_user, schema_registry_pwd):
+    def __init__(self, 
+                 schema_registry_url: str, 
+                 schema_registry_user: str, 
+                 schema_registry_pwd: str):
         config = dict()
         config['url'] = schema_registry_url
         config['basic.auth.user.info'] = '{usr}:{pwd}'.format(usr=schema_registry_user, pwd=schema_registry_pwd)
         self.sr_client = SchemaRegistryClient(config)
 
-    def get_latest_version_schema(self, data_tag):
+    def get_latest_version_schema(self, 
+                                  data_tag: str) -> str:
         data_tag = data_tag + '-value'
         schema_object = self.sr_client.get_latest_version(data_tag)
         return schema_object.schema.schema_str
